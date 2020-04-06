2ba7b6fad2a850f83423d85ac8d5a3eadb357875
willfbren <willfbren@gmail.com>
2020-04-06 16:56:01 -0500

    Done.

--- app/controllers/ducks_controller.rb
+++ app/controllers/ducks_controller.rb
@@ -4,4 +4,31 @@ class DucksController < ApplicationController
     @ducks = Duck.all
   end
 
+  def show
+    @duck = Duck.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    if params[:name] != "" && params[:description] != ""
+      @duck = Duck.create(name: params[:name], description: params[:description], student_id: params[:student_id]) 
+    end
+
+    redirect_to "/ducks/#{@duck.id}"
+  end
+
+  def edit
+    @duck = Duck.find(params[:id])
+  end
+
+  def update
+    @duck = Duck.find(params[:id])
+    if params[:name] != "" && params[:description] != ""
+      @duck.update(name: params[:name], description: params[:description], student_id: params[:student_id])
+    end
+    redirect_to "/ducks/#{@duck.id}"
+  end
+
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,4 +4,29 @@ class StudentsController < ApplicationController
     @students = Student.all
   end
 
+  def show
+    @student = Student.find(params[:id])
+    @ducks = Duck.where(student_id: @student.id)
+  end
+
+  def new
+  end
+
+  # need to check if name is blank and mod is between 1 & 5
+  def create
+    @student = Student.create(name: params[:name], mod: params[:mod])
+
+    redirect_to "/students/#{@student.id}"
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    @student.update(name: params[:name], mod: params[:mod])
+
+    redirect_to "/students/#{@student.id}"
+  end
 end
