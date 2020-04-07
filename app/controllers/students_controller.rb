5589a5d5bf449c08d63efc466d74bd46bbca32f8
AidanMcB <aidankmcbride@gmail.com>
2020-04-06 22:35:35 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,4 +4,36 @@ class StudentsController < ApplicationController
     @students = Student.all
   end
 
+  def show 
+    @student = Student.find(params[:id])
+  end
+
+  def new
+    @student = Student.new
+    render :new
+  end
+  
+  def create
+    @student = Student.create({
+      name: params[:name],
+      mod: params[:mod]
+    })
+    redirect_to("/students/#{@student.id}")
+  end
+
+  def edit 
+    @student = Student.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    @student.update({
+      name: params[:name],
+      mod: params[:mod]
+    })
+    redirect_to("/students/#{@student.id}")
+  end    
+
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  resources :ducks, only: [:index]
-  resources :students, only: [:index]
+  resources :ducks, only: [:index, :show, :edit, :update, :new, :update]
+  resources :students, only: [:index, :show, :new, :edit, :update]
+  post '/students/create', to: 'students#create'
+  get '/students/:id/update', to: 'students#update'
+  post '/ducks/create', to: 'ducks#create'
+  get 'ducks/:id/update', to: 'ducks#update'
+  
 end
