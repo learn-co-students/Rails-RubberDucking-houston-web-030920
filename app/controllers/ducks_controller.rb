0ab68a34ef2b04685dbd73938df7356d479744a9
ezrablack <timothydanielmorales@gmail.com>
2020-04-07 21:52:42 -0500

    Done.

--- app/controllers/ducks_controller.rb
+++ app/controllers/ducks_controller.rb
@@ -4,4 +4,35 @@ class DucksController < ApplicationController
     @ducks = Duck.all
   end
 
+  def new
+    @students = Student.all
+  end 
+
+  def create 
+    @new_duck = Duck.create({name: params[:name], 
+                            description: params[:description], 
+                            student_id: Student.all.sample.id})
+    redirect_to('/ducks')
+  end 
+
+  def show
+    @duck = Duck.find(params[:id])
+    @student = Student.find_by(id: @duck.student_id)
+  end 
+
+  def edit
+    @ducks = Duck.all
+    @duck = Duck.find_by(id: params[:id])
+    @students = Student.all
+  end 
+
+  def update
+    @duck = Duck.find_by(id: params[:id])
+    @duck.update({name: params[:name], 
+    description: params[:description], 
+    student_id: params[:student_id]})
+    redirect_to ('/ducks')
+  end 
+
+
 end
