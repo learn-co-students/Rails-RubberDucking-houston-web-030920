68dd42e80cd5ff58d00ac6262ae53e6b3e609624
RafaS92 <manrvaldez..92@gmail.com>
2020-04-09 20:19:35 -0500

    Done.

--- app/controllers/ducks_controller.rb
+++ app/controllers/ducks_controller.rb
@@ -1,7 +1,40 @@
 class DucksController < ApplicationController
 
+ 
+
   def index
     @ducks = Duck.all
+  end
+
+  def new
+
+  end
+
+  def create
+    @duck = Duck.create({
+      name: params[:name],
+      description: params[:description],
+      student_id: params[:student_id]
+    })
+    redirect_to "/ducks"
+  end
+  
+  def show
+    @duck = Duck.find(params[:id])
+  end
+
+  def edit
+    
+    @duck = Duck.find(params[:id])
+  end
+
+  def update
+    @duck = Duck.find(params[:id])
+    @duck.update({
+      name: params[:name],
+      description: params[:description]
+    })
+    redirect_to "/ducks"
   end
 
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,6 +2,41 @@ class StudentsController < ApplicationController
 
   def index
     @students = Student.all
+    
   end
+
+  def show
+    @student = Student.find(params[:id])
+
+  end
+
+  def new
+    
+  end
+
+  def create
+   
+    @student = Student.create({
+      name: params[:name],
+      mod: params[:mod]
+    })
+    redirect_to('/students')
+  end
+
+  def edit
+    @student = Student.find_by({id: params[:id]})
+    render :edit
+  end
+
+  def update
+  
+  @student = Student.find_by({ id: params[:id]})
+  @student.update({
+    name: params[:name],
+    mod: params[:mod]
+  })
+  redirect_to("/students")
+  end
+
 
 end
