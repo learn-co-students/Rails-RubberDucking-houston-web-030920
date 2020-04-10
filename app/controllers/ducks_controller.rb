cc2f783b679c31c1635fe4ff9f29167060319305
faizah1992 <faizah1992@gmail.com>
2020-04-09 21:50:31 -0500

    Done.

--- app/controllers/ducks_controller.rb
+++ app/controllers/ducks_controller.rb
@@ -4,4 +4,40 @@ class DucksController < ApplicationController
     @ducks = Duck.all
   end
 
+  def show 
+    @duck = Duck.find(params[:id])
+    @student = @duck.student
+  end
+
+  def edit
+    @students = Student.all
+    @duck = Duck.find(params[:id])
+  end
+
+  def update
+    @duck = Duck.find(params[:id])
+    @duck.update({name:params[:name],
+      description: params[:description
+      ]})
+      redirect_to"/ducks"
+
+  end
+
+  def new
+    @students = Student.all
+  end
+
+  def create 
+    @duck = Duck.create({
+      name: params[:name],
+      description: params[:description],
+      student_id: params[:student_id]
+    })
+    redirect_to"/ducks"
+  end
+
+
+
+
+
 end
